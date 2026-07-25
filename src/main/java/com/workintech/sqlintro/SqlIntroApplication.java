86eece565de33cd04a1c7baf258f4c9089a44c12
Talha <talhaakin094@gmail.com>
2026-07-25 22:55:52 +0300

    completed

--- src/main/java/com/workintech/sqlintro/SqlIntroApplication.java
+++ src/main/java/com/workintech/sqlintro/SqlIntroApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SqlIntroApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SqlIntroApplication.class, args);
 	}
-
 }
